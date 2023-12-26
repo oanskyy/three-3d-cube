2d1cec6ba3939218cec6b8b3665b951e773dbb68
oanskyy <oana.anghelachi@gmail.com>
2023-12-26 02:57:15 +0200

    perf: add anti-aliasing

--- src/script.js
+++ src/script.js
@@ -1,57 +1,50 @@
-import * as THREE from "three"
-import { OrbitControls } from "three/addons/controls/OrbitControls.js"
-
-console.log(OrbitControls)
+import * as THREE from "three";
+import { OrbitControls } from "three/addons/controls/OrbitControls.js";
 
 // initialize the scene
-const scene = new THREE.Scene()
+const scene = new THREE.Scene();
 
 // add objects to the scene
-const cubeGeometry = new THREE.BoxGeometry(1, 1, 1)
-const cubeMaterial = new THREE.MeshBasicMaterial({ color: "red" })
+const cubeGeometry = new THREE.BoxGeometry(1, 1, 1);
+const cubeMaterial = new THREE.MeshBasicMaterial({ color: "red" });
 
-const cubeMesh = new THREE.Mesh(cubeGeometry, cubeMaterial)
-scene.add(cubeMesh)
+const cubeMesh = new THREE.Mesh(cubeGeometry, cubeMaterial);
+scene.add(cubeMesh);
 
 // initialize the camera
-// const camera = new THREE.PerspectiveCamera(
-// 	75,
-// 	window.innerWidth / window.innerHeight,
-// 	0.1,
-// 	200
-// )
+const camera = new THREE.PerspectiveCamera(
+  35,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  200
+);
 
-const aspectRatio = window.innerWidth / window.innerHeight
-
-const camera = new THREE.OrthographicCamera(
-	-1 * aspectRatio,
-	1 * aspectRatio,
-	1,
-	-1,
-	0.1,
-	200
-)
-// scene.add( camera );
-camera.position.z = 5
+camera.position.z = 5;
 
 // initialize the renderer
-const canvas = document.querySelector("canvas.threejs")
+const canvas = document.querySelector("canvas.threejs");
 const renderer = new THREE.WebGLRenderer({
-	canvas: canvas
-})
-renderer.setSize(window.innerWidth, window.innerHeight)
+  canvas: canvas,
+  antialias: true,
+});
+renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 
 // instantiate the controls
-const controls = new OrbitControls(camera, canvas)
-controls.enableDamping = true
-controls.autoRotate = true
+const controls = new OrbitControls(camera, canvas);
+controls.enableDamping = true;
+// controls.autoRotate = true;
 
+window.addEventListener('resize', () =>{
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix()
+  renderer.setSize(window.innerWidth, window.innerHeight);
+})
+
+// render the scene
 const renderloop = () => {
-	// required if controls.enableDamping or controls.autoRotate are set to true
-	controls.update()
-	renderer.render(scene, camera)
-	window.requestAnimationFrame(renderloop)
-}
+  controls.update();  renderer.render(scene, camera);
+  window.requestAnimationFrame(renderloop);
+};
 
-renderloop()
-// window.requestAnimationFrame()
+renderloop();
